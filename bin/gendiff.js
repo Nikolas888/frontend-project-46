709aac7f1a1551deb3332d5cc2a4f69108d07cf9
Nikolas <nik.dekonte@gmail.com>
2022-11-25 09:40:47 +0300

    112

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -19,7 +19,12 @@ program.parse(process.argv);
 const options = program.opts();
 // if (options.debug) console.log(options + '123');
 if (options.version) console.log('version -> 1.0.0');
-if (options.help) console.log('display help for command');
+if (options.help) console.log('  Usage: gendiff [options]\n');
+if (options.help) console.log('  Compares two configuration files and shows a difference.\n');
+if (options.help) console.log('  Options:');
+if (options.help) console.log('    -V, --version        output the version number');
+if (options.help) console.log('    -h, --help           display help for command');
+
 // console.log('pizza details:');
 // if (options.small) console.log('- small pizza size2');
 // if (options.pizzaType) console.log(`- ${options.pizzaType}`);
