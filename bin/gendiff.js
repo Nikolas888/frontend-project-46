ebe1d27c911cc834858d362e29942d9a52d0c18d
Nikolas <niksan540@gmail.com>
2022-12-06 15:32:24 +0300

    124

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,71 +1,69 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-var fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 
 program
   // .option('-v, --version', 'output the version number')
   // .version('0.1.0')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
   // .option('-h, --help', 'display help for command')
   // .description('An application for pizza ordering')
   // .action(console.log('112'))
   .argument('<filepath1>', 'path to first file.json')
   .argument('<filepath2>', 'path to second file.json')
-  .action(gendiff = (filepath1, filepath2) => {
-    
+  .action((filepath1, filepath2) => {
     // Читаем первый файл и парсим его превращая в обьект
-    const obj1 = JSON.parse(fs.readFileSync(path.resolve(filepath1))); 
-    
+    const obj1 = JSON.parse(fs.readFileSync(path.resolve(filepath1)));
+
     // Читаем второй файл и парсим его превращая в обьект
-    const obj2 = JSON.parse(fs.readFileSync(path.resolve(filepath2))); 
-    
+    const obj2 = JSON.parse(fs.readFileSync(path.resolve(filepath2)));
+
     // Получаем вложенный массив [ключ, значение] из обьектов
     const m1 = Object.entries(obj1);
     const m2 = Object.entries(obj2);
-    
+
     // Получаем  массив ключей из обьектов
-    const m1Keys = Object.keys(obj1); 
+    const m1Keys = Object.keys(obj1);
     const m2Keys = Object.keys(obj2);
-    
+
     const result = [];
     let finish = '';
-    
-    // Сравниваем первый массив со вторым. 
-    for (const [key1, value1] of m1) {
-      for (const [key2, value2] of m2) {
-    
-        //Если ключ и значение совпадают. 
-    if (key1 === key2 && value1 === value2) {
-      result.push(`  ${key1}: ${value1}`)
-    }
-    
-    // Если ключ совпадает, а значение нет. 
-    else if (key1 === key2 && value1 !== value2) {
-      result.push(`- ${key1}: ${value1}`);
-      result.push(`+ ${key1}: ${value2}`)
-    }
+
+    // Сравниваем первый массив со вторым.
+    for (const [keyfirst, valuefirst] of m1) {
+      for (const [keysecond, valuesecond] of m2) {
+        // Если ключ и значение совпадают.
+        if (keyfirst === keysecond && valuefirst === valuesecond) {
+          result.push(`  ${keyfirst}: ${valuefirst}`);
+        }
+
+        // Если ключ совпадает, а значение нет.
+        else if (keyfirst === keysecond && valuefirst !== valuesecond) {
+          result.push(`- ${keyfirst}: ${valuefirst}`);
+          result.push(`+ ${keyfirst}: ${valuesecond}`);
+        }
       }
-    
+
       // Если 2й массив не содержит ключ 1го массива
-      if (!m2Keys.includes(key1)) {
-        result.push(`- ${key1}: ${value1}`);
+      if (!m2Keys.includes(keyfirst)) {
+        result.push(`- ${keyfirst}: ${valuefirst}`);
       }
     }
-    
+
     // Сравниваем второй массив с первым.
-    
-    for (const [key2, value2] of m2) {
+
+    for (const [keysecond, valuesecond] of m2) {
       // Если 1й массив не содержит ключ 2го массива
-      if (!m1Keys.includes(key2)) {
-        result.push(`+ ${key2}: ${value2}`);
+      if (!m1Keys.includes(keysecond)) {
+        result.push(`+ ${keysecond}: ${valuesecond}`);
       }
     }
-    
+
     // Сортируем массив результата по 3му сиволу пузырьковой сортировкой
-    const bubbleSort = (coll) => {
-      let stepsCount = coll.length - 1;
+    const bubbleSort = (x) => {
+      let stepsCount = x.length - 1;
       // Объявляем переменную swapped, значение которой показывает,
       // был ли совершен обмен элементов во время перебора массива
       let swapped;
@@ -77,11 +75,11 @@ program
         // Перебираем массив и меняем местами элементы, если предыдущий
         // больше, чем следующий
         for (let i = 0; i < stepsCount; i += 1) {
-          if (coll[i][2] > coll[i + 1][2]) {
+          if (x[i][2] > x[i + 1][2]) {
             // temp – временная константа для хранения текущего элемента
-            const temp = coll[i];
-            coll[i] = coll[i + 1];
-            coll[i + 1] = temp;
+            const temp = x[i];
+            x[i] = x[i + 1];
+            x[i + 1] = temp;
             // Если сработал if и была совершена перестановка,
             // присваиваем swapped значение true
             swapped = true;
@@ -91,21 +89,20 @@ program
         // в конце массива
         stepsCount -= 1;
       } while (swapped); // продолжаем, пока swapped === true
-    
-      return coll;
+
+      return x;
     };
-    
-    //Запускаем пузырьковую сортировку и изменяем исходный массив
+
+    // Запускаем пузырьковую сортировку и изменяем исходный массив
     bubbleSort(result);
 
-    //Преобразуем массив в строку
+    // Преобразуем массив в строку
     for (const i of result) {
-      finish = finish + i + '\n';
+      finish = `${finish + i}\n`;
     }
     finish = `{\n${finish}}`;
 
     console.log(finish);
-    })
+  });
 
 program.parse();
-
