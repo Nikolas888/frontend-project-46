5c71d0769f871d9e437530e2047f01334bbd09ed
Nikolas <niksan540@gmail.com>
2023-02-10 11:53:26 +0300

    10.02.2023/11-53

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -14,5 +14,5 @@ test('gendiff', () => {
 //   }
   const res = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
   /* eslint-disable-next-line */
-  expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(res);
+  expect(gendiff('__fixtures__/file01.json', '__fixtures__/file02.json')).toEqual(res);
 });
