a8d646525333d9b816ce7d77206bf4cdbfdd8f0c
Nikolas <niksan540@gmail.com>
2023-02-16 14:01:12 +0300

    12

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,22 +6,16 @@ import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-console.log(getFixturePath('expectedResultStylish.txt'));
+// console.log(getFixturePath('expectedResultStylish.txt'));
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-// console.log(readFile('expectedResultStylish.txt'));
-
 
 const expectedResultStylish = readFile('expectedResultStylish.txt');
 const expectedResultPlain = readFile('expectedResultPlain.txt');
 const expectedResultJson = readFile('expectedResultJson.txt');
-
-// console.log(expectedResultStylish);
-// console.log(expectedResultPlain);
-// console.log(expectedResultJson);
 
 const formatsFiles = ['json', 'yaml', 'yml'];
 
@@ -29,10 +23,12 @@ test.each(formatsFiles)('diff formats of files (.json .yaml .yml)', (extension) 
   const fileName1 = `${process.cwd()}/__fixtures__/file1.${extension}`;
   const fileName2 = `${process.cwd()}/__fixtures__/file2.${extension}`;
 
-  expect(gendiff(fileName1, fileName2, 'stylish')).toEqual(expectedResultStylish);
+  // expect(gendiff(fileName1, fileName2, 'stylish')).toEqual(expectedResultStylish);
   // expect(gendiff(fileName1, fileName2, 'plain')).toEqual(expectedResultPlain);
   // expect(gendiff(fileName1, fileName2, 'json')).toEqual(expectedResultJson);
   // expect(gendiff(fileName1, fileName2)).toEqual(expectedResultStylish);
+  console.log('1' + fileName1);
+  console.log('2' + fileName2);
 });
 
 /* eslint-disable-next-line */
@@ -50,5 +46,5 @@ test.each(formatsFiles)('diff formats of files (.json .yaml .yml)', (extension) 
 //   }
 // const res = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
 /* eslint-disable-next-line */
-  // expect(gendiff('__fixtures__/file01.json', '__fixtures__/file02.json')).toEqual(res);
+  // expect(gendiff('/home/nik/projects/frontend-project-46/__fixtures__/file01.json', '/home/nik/projects/frontend-project-46/__fixtures__/file02.json')).toEqual(res);
 // });
